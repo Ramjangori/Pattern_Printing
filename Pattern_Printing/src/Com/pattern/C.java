4ddd3258655600142b18aad4c0196cc070f1f032
MOH RAMJAN <ramjangori6666@gmail.com>
2026-01-12 22:50:34 +0530

    Albhabetic_Pattern_Printing

---
+++ Pattern_Printing/src/Com/pattern/C.java
@@ -0,0 +1,20 @@
+package Com.pattern;
+
+public class C {
+	public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i==0 || i==n-1 || j==0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
