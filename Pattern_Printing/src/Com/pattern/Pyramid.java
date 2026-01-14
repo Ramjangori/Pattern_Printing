43594ddaae0aabb8b79eab54191b0a152aa54105
MOH RAMJAN <ramjangori6666@gmail.com>
2026-01-14 23:07:09 +0530

    Pyramid

---
+++ Pattern_Printing/src/Com/pattern/Pyramid.java
@@ -0,0 +1,21 @@
+package Com.pattern;
+
+public class Pyramid {
+	public static void main(String[] args) {
+		 int n = 21;
+
+	     for (int i = 0; i < n; i++) {
+	         for (int j = 0; j < n; j++) {
+
+	             if (i+j==(n-1)/2||j-i==(n-1)/2||i==(n-1)/2) {
+	                 System.out.print("*");
+	             } else {
+	                 System.out.print(" ");
+	             }
+
+	         }
+	         System.out.println();
+	     }
+	}
+ 
+}
